a9937d1a6ad07f27ce53ba733a08339e765cfc85
Fredrick Ochieng <flaughters@gmail.com>
2023-02-07 17:26:48 +0300

    all_needed_stuff for day 3✔✔🎁

---
+++ README.md
@@ -0,0 +1,80 @@
+## RollerCoaster & Odd/Even Checker & BMI 2.0 & Leap Year Checker & Pizza Order Program & Treasure Island Game
+
+This program is a combination of 6 different mini-programs, each with a different purpose. The first program is a RollerCoaster payment system that calculates the amount a person should pay for a ride based on their height and age. The second program checks whether a number entered is even or odd. The third program calculates the Body Mass Index (BMI) of a person based on their height and weight. The fourth program checks if a year entered is a leap year or not. Finally, the last program is an automatic pizza order system that calculates the price of a pizza order based on its size, toppings, and extra cheese.
+##  Treasure Island Game
+
+Welcome to Treasure Island! Your mission is to find the treasure. You'll be given choices at each step, and your decisions will determine whether you win or lose the game.
+How to play
+
++ Run the code in your Python environment.
++ You'll be at a cross road. Choose either "left" or "right".
++ Based on your choice, you'll face different challenges. Choose wisely to reach the treasure.
++ If you enter a door of the wrong color, you lose the game. If you choose the yellow door, you win!
+
+Good luck on your treasure hunt!
+
+## Other programs included
+
+## Requirements
+
++ Python 3
++ A basic understanding of programming concepts such as variables, input/output, and control structures (if/else)
+
+## Installation
+
+1. Clone the repository:
+
+        $ https://github.com/Freddy10035/Treasure-Island.git
+
+2. Navigate to the directory:
+
+        $ cd Treasure-Island
+
+## Usage
+
+1. RollerCoaster Payment
+
+    To run the RollerCoaster payment program, simply execute the following command in your terminal:
+
+        $ python rollercoaster.py
+
+    The program will prompt you for your height and name, and based on your age, it will calculate the amount you have to pay.
+
+
+2. Odd/Even Checker
+
+    To run the odd/even checker program, simply execute the following command in your terminal:
+
+        $ python odd_even.py
+
+    The program will prompt you for a number, and it will then check if the number is odd or even.
+
+
+3. BMI 2.0
+
+    To run the BMI 2.0 program, simply execute the following command in your terminal:
+
+       $ python bmi.py
+
+    The program will prompt you for your height and weight, and it will then calculate your BMI and classify it into one of the five categories: underweight, normal weight, slightly overweight, obese, or clinically obese.
+
+
+4. Leap Year Checker
+
+    To run the leap year checker program, simply execute the following command in your terminal:
+
+        $ python leap_year.py
+
+    The program will prompt you for a year, and it will then check if the year is a leap year or not.
+
+
+5. Pizza Order Program
+
+    To run the pizza order program, simply execute the following command in your terminal:
+
+       $ python pizza_order.py
+
+    The program will prompt you for the size of the pizza, whether you want pepperoni and/or extra cheese, and it will then calculate the total cost of the order.
+## Contributing
+
+Feel free to contribute to this project by submitting a pull request. Any contributions, big or small, are welcome!
--- main.py
+++ main.py
@@ -166,12 +166,75 @@
 #
 # if score_to_int < 10 or score_to_int > 90:
 #     print(f"Your score is {score_to_int}, you go together like coke and mentos.")
-# elif 40 <= score_to_int <= 50:
+# elif 40 >= score_to_int <= 50:
 #     print(f"Your score is {score_to_int}, you are alright together.")
 # else:
 #     print(f"Your score is {score_to_int}.")
 
 #################################################################################
-# automatic pizza order program.
+# Treasure Island
 #################################################################################
+
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+# https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+# Write your code below this lin    e 👇
+print("Welcome to Treasure Island.\nYour mission is to find the treasure")
+direction = input('You are at a cross road. Where do you want to go? "left" or "right"\n')
+
+if direction == "right":
+    print("You fall into a hole. GAME OVER!!")
+    exit()
+
+elif direction == "left":
+    swim_wait = input('You come to a lake. There is an island in the middle of the lake.'
+                      'Type "wait" to wait for a boat. Type "swim" to swim across.\n')
+
+    if swim_wait == "swim" or "":
+        print("You are attacked by Trouts. GAME OVER!!")
+        exit()
+
+    elif swim_wait == "wait":
+        color = input('You arrive at the island unharmed. There is a house with 3 doors. '
+                      'One red, one yellow and one blue. Which color do yov choose?\n')
+
+        if color == "yellow":
+            print("You WIN!!")
+        elif color == "blue":
+            print("You enter a room of beasts. GAME OVER!!")
+            exit()
+        elif color == "red":
+            print("You enter a room of fire an gets burned. GAME OVER!!")
+            exit()
+        else:
+            print("GAME OVER!!")
+            exit()
+    else:
+        print("You are attacked by Trouts. GAME OVER!!")
+else:
+    print("You fall into a hole. GAME OVER!!")
 
